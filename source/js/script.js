7f291e0246fccb8404c048eba8132d25647a75dc
Guus Goossens <guus.goossens@gmail.com>
2011-08-22 11:04:14 +0200

    Openen en sluiten van het menu

--- source/js/script.js
+++ source/js/script.js
@@ -11,3 +11,16 @@ $('nav#NavPanel .backButton').click(function() {
 	$('nav#NavPanel ul li ul.show').removeClass('show');
 	$('nav#NavPanel .backButton').hide();
 });
+
+/*
+	Knop om het menu te openen en sluiten
+*/
+$('#menuButton').click( function() {
+	var $navPanel = $('#NavPanel');
+	if ( $navPanel.hasClass('show') ) {
+		$navPanel.removeClass('show');
+	} else {
+		$navPanel.addClass('show');
+	}
+	
+});
